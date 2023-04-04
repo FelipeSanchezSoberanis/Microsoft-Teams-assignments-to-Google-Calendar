5692f7920b9a2236c2521bd4f9c3600d008a5991
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-04-04 12:07:22 -0600

    Initial commit

---
+++ src/test/java/com/felipe/teams/TeamsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.felipe.teams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
